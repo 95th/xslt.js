1218f5b25032dbf36c30887cd7b7ba58415bd8c7
Gurwinder Singh <vargwin@gmail.com>
2021-02-26 07:49:48 +0530

    Rename module var

--- src/wrapper.js
+++ src/wrapper.js
@@ -5,7 +5,7 @@ var Xslt = {
 (async () => {
     /* XSLT.RAW.JS */
 
-    let module = await createXsltModule();
+    let xsltModule = await createXsltModule();
     Xslt = Object.freeze({
         transform: (xsltFile, xml) => {
             if (!xsltFile) {
@@ -16,20 +16,20 @@ var Xslt = {
                 throw "Empty XML String";
             }
 
-            var xsl_ptr = module.allocateUTF8(xsltFile);
-            var xml_ptr = module.allocateUTF8(xml);
+            var xsl_ptr = xsltModule.allocateUTF8(xsltFile);
+            var xml_ptr = xsltModule.allocateUTF8(xml);
             try {
-                var output_ptr = module._xsltTransform(xsl_ptr, xml_ptr);
+                var output_ptr = xsltModule._xsltTransform(xsl_ptr, xml_ptr);
                 if (!output_ptr) {
                     throw "Transformation failed";
                 }
 
-                let output = module.UTF8ToString(output_ptr);
-                module._free(output_ptr);
+                let output = xsltModule.UTF8ToString(output_ptr);
+                xsltModule._free(output_ptr);
                 return output;
             } finally {
-                module._free(xsl_ptr);
-                module._free(xml_ptr);
+                xsltModule._free(xsl_ptr);
+                xsltModule._free(xml_ptr);
             }
         }
     });
