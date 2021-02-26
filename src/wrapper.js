2f52534e1e0d3bcee3f43a896a265f60a322c970
Gurwinder Singh <vargwin@gmail.com>
2021-02-26 12:29:25 +0530

    Make script includable in IE

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,13 +1,16 @@
-var Xslt = {
-    transform: () => { throw "XSLT engine not initialized" }
-};
+var Xslt = null;
 
-(async () => {
+(function () {
+    if (typeof WebAssembly !== "object" || typeof WebAssembly.instantiate !== "function") {
+        return;
+    }
+
+    var Module = {};
+
     /* XSLT.RAW.JS */
 
-    let xsltModule = await createXsltModule();
     Xslt = Object.freeze({
-        transform: (xsltFile, xml) => {
+        transform: function (xsltFile, xml) {
             if (!xsltFile) {
                 throw "Empty XSLT file name";
             }
@@ -16,20 +19,20 @@ var Xslt = {
                 throw "Empty XML String";
             }
 
-            var xsl_ptr = xsltModule.allocateUTF8(xsltFile);
-            var xml_ptr = xsltModule.allocateUTF8(xml);
+            var xsl_ptr = Module.allocateUTF8(xsltFile);
+            var xml_ptr = Module.allocateUTF8(xml);
             try {
-                var output_ptr = xsltModule._xsltTransform(xsl_ptr, xml_ptr);
+                var output_ptr = Module._xsltTransform(xsl_ptr, xml_ptr);
                 if (!output_ptr) {
                     throw "Transformation failed";
                 }
 
-                let output = xsltModule.UTF8ToString(output_ptr);
-                xsltModule._free(output_ptr);
+                let output = Module.UTF8ToString(output_ptr);
+                Module._free(output_ptr);
                 return output;
             } finally {
-                xsltModule._free(xsl_ptr);
-                xsltModule._free(xml_ptr);
+                Module._free(xsl_ptr);
+                Module._free(xml_ptr);
             }
         }
     });
