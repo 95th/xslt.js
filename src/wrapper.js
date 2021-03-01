8da7102e611de78a459208c3671adde1d319296a
Gurwinder Singh <vargwin@gmail.com>
2021-03-01 09:53:10 +0530

    use ActiveXObject if IE

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,23 +1,44 @@
-var Xslt = null;
+var xsltTransform = (function () {
+    function validateArgs(xsltFile, xml) {
+        if (!xsltFile) {
+            throw "Empty XSLT file name";
+        }
 
-(function () {
-    if (typeof WebAssembly !== "object" || typeof WebAssembly.instantiate !== "function") {
-        return;
+        if (!xml) {
+            throw "Empty XML String";
+        }
     }
 
-    var Module = {};
+    if ("ActiveXObject" in window) {
+        return function (xsltFile, xml) {
+            validateArgs(xsltFile, xml);
 
-    /* XSLT.RAW.JS */
+            var xsltDoc = new ActiveXObject("MSXML2.DOMDocument.6.0");
+            xsltDoc.setProperty("AllowXsltScript", true);
+            xsltDoc.setProperty("AllowDocumentFunction", true);
+            xsltDoc.resolveExternals = true;
+            xsltDoc.async = false;
+            xsltDoc.load(xsltFile);
 
-    Xslt = Object.freeze({
-        transform: function (xsltFile, xml) {
-            if (!xsltFile) {
-                throw "Empty XSLT file name";
-            }
+            var xmlDoc = new ActiveXObject("MSXML2.DOMDocument.6.0");
+            xmlDoc.setProperty("AllowXsltScript", true);
+            xmlDoc.setProperty("AllowDocumentFunction", true);
+            xmlDoc.resolveExternals = true;
+            xmlDoc.preserveWhiteSpace = true;
+            xmlDoc.async = false;
+            xmlDoc.loadXML(xml);
 
-            if (!xml) {
-                throw "Empty XML String";
-            }
+            return xmlDoc.transformNode(xsltDoc.documentElement);
+        };
+    }
+
+    if (typeof WebAssembly === "object" && typeof WebAssembly.instantiate === "function") {
+        var Module = {};
+
+        /* XSLT.RAW.JS */
+
+        return function (xsltFile, xml) {
+            validateArgs(xsltFile, xml);
 
             var xsl_ptr = Module.allocateUTF8(xsltFile);
             var xml_ptr = Module.allocateUTF8(xml);
@@ -35,5 +56,7 @@ var Xslt = null;
                 Module._free(xml_ptr);
             }
         }
-    });
+    }
+
+    throw "XSLT transformation is not supported on this browser";
 })();
