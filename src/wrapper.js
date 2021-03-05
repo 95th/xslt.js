537690608b001f6053295f5236382b22fd813ff1
Gurwinder Singh <vargwin@gmail.com>
2021-03-05 21:29:55 +0530

    Create XsltEngine class

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,11 +1,10 @@
-var xsltTransform;
-(async () => {
-    /* XSLT.RAW.JS */
+class XsltEngine {
+    constructor(mod) {
+        this.mod = mod;
+        this.fn = mod.cwrap("xsltJsTransform", "number", ["number", "number"], { async: true });
+    }
 
-    let mod = await createXsltModule();
-    let xsltJsTransform = mod.cwrap("xsltJsTransform", "number", ["number", "number"], { async: true });
-
-    xsltTransform = async (xsltFile, xml) => {
+    async transform(xsltFile, xml) {
         if (!xsltFile) {
             throw "Empty XSLT file name";
         }
@@ -14,20 +13,27 @@ var xsltTransform;
             throw "Empty XML String";
         }
 
-        let xsltPtr = mod.allocateUTF8(xsltFile);
-        let xmlPtr = mod.allocateUTF8(xml);
+        let xsltPtr = this.mod.allocateUTF8(xsltFile);
+        let xmlPtr = this.mod.allocateUTF8(xml);
         try {
-            let outputPtr = await xsltJsTransform(xsltPtr, xmlPtr);
+            let outputPtr = await this.fn(xsltPtr, xmlPtr);
             if (!outputPtr) {
                 throw "Transformation failed";
             }
 
-            let output = mod.UTF8ToString(outputPtr);
-            mod._free(outputPtr);
+            let output = this.mod.UTF8ToString(outputPtr);
+            this.mod._free(outputPtr);
             return output;
         } finally {
-            mod._free(xsltPtr);
-            mod._free(xmlPtr);
+            this.mod._free(xsltPtr);
+            this.mod._free(xmlPtr);
         }
-    };
-})();
+    }
+
+    static async create() {
+        /* XSLT.RAW.JS */
+
+        let mod = await createXsltRawModule();
+        return new XsltEngine(mod);
+    }
+}
